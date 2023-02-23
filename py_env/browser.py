2e5afc3a56ee326ee132b11390e85ac4fb719b2d
Ubuntu <ubuntu@ip-172-31-55-116.ec2.internal>
2023-02-23 13:18:24 +0000

    screenshot from ec2

--- py_env/browser.py
+++ py_env/browser.py
@@ -15,12 +15,27 @@ import os
 from dotenv import load_dotenv
 import math
 import json
+from pyvirtualdisplay import Display
 
 def launchChrome():
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    driver.get('https://main.d2l3seqs559cm7.amplifyapp.com/')
-    print(driver.page_source)
-    driver.quit()
+    display = Display(visible=0, size=(800,800))
+    display.start()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('headless')
+    chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' 'Chrome/97.0.4692.71 Safari/537.36')
+    driver = webdriver.Chrome(service=Service("/home/ubuntu/chromedriver"), options=chrome_options)
+    driver.get('https://www.google.com/search?q=kings+bridge+auto&rlz=1C5GCEM_enCA1032CA1032&sxsrf=AJOqlzWrO_2TEnEQwAbmqIhLRgBTqz-Dmw%3A1676307742594&ei=Hm3qY73vI7CdptQPpMCYwAc&ved=0ahUKEwi99p_8_JL9AhWwjokEHSQgBngQ4dUDCA8&uact=5&oq=kings+bridge+auto&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIECCMQJzIECCMQJzIQCC4QgAQQFBCHAhDHARCvATILCAAQFhAeEPEEEAoyCwgAEBYQHhDxBBAKMgsIABAWEB4Q8QQQCjIICAAQFhAeEAoyAggmMgUIABCGAzIFCAAQhgM6CggAEEcQ1gQQsANKBAhBGABKBAhGGABQqgdYqgdgsgloAnABeACAAYQBiAGEAZIBAzAuMZgBAKABAcgBBMABAQ&sclient=gws-wiz-serp#lrd=0x4b0ca3c1857b5645:0x7a7a0b75909dffd7,1,,,,')
+    driver.maximize_window()
+    driver.save_screenshot('screenshot.png')
+    driver.implicitly_wait(10)
+    try:
+        WebDriverWait(driver, 10).until(lambda driver: driver.execute_script('return document.readyState') == 'complete') 
+        print(driver.page_source)
+        display.stop()
+
+        driver.quit()
+    except Exception as err:
+        print(err)
+        print('there is no element')
 
 launchChrome()
